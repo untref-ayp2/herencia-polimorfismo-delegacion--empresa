2e1af3c1c0d1a3ce61295bcea38a62682fbaa3fb
Leandro Doctors <ldoctors@untref.edu.ar>
2021-04-29 16:17:06 -0300

    Empleados Sin Categoría: agregar tipo de Jornada
    Además de agregar el atributo, también debemos modificar "toString()" en
    la subclase para que muestre este nuevo atributo.
    
    *****
    
    Notese que al redefinir toString() agregamos la anotación "@Override".
    (Esta anotación es una directiva al compilador, para que verifique que
    realmente estamos sobreescribiendo un método de una superclase.)
    
    Probablemente se pregunten por qué hace falta explicitar esa directiva:
    (al fin de al cabo, el programa compila igual si no lo hacemos...).
    Simple: consideremoslo un seguro de caución. Veamos...
    
    Veamos qué pasa si nos equivocamos y, en lugar de escribir "toString()"
    (con la S mayúscula), escribimos "tostring()" (con todo en minúsculas).
    
    a) Si agregamos la directiva, el compilador se va a asegurar que
    efectivamente
    estemos sobreescribiendo un método ya existente. Si no es así, el
    programa
    no va a compilar, y fácilmente nos vamos a dar cuenta de nuestro error.
    
    b) Si no lo hacemos, vamos a generar dos problemas:
    
    - nuestro "toString()" va a ser erróneo (al fin de al cabo, nunca lo
    sobreescribimos...)
    - vamos a generar un nuevo método "tostring()"
    
    Luego:
    
    - nuestras invocaciones al método adecuado van a dar un error (que,
    probablemente, va a ser bastante difícil de ser detectado).
    - si por alguna razón invocamos al método erróneo, vamos a experimentar
    comportamientos probablemente muy diferentes... por invocaciones muy
    similares.
    
    Todo esto va a hacernos muy difícil el poder detectar y corregir este
    error,
    generandonos muchos dolores de cabeza.
    
    Conclusión: evitemos subestimar las anotaciones :-)

--- src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/SinCategoria.java
+++ src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/SinCategoria.java
@@ -1,9 +1,18 @@
 package ar.edu.untref.ayp2.polimorfismo.empresa;
 
 public class SinCategoria extends Empleado {
+	private String jornada;
 
-	public SinCategoria(String nombre, String categoria, String planta) {
+	public SinCategoria(String nombre, String categoria, String planta, String jornada) {
 		super(nombre, categoria, planta);
+		this.jornada = jornada;
+	}
+
+	@Override
+	public String toString() {
+		String catPyJ = String.join(", ", categoria, planta, jornada);
+		catPyJ = "(" + catPyJ + ")";
+		return String.join(" ", nombre, catPyJ);
 	}
 
 }
