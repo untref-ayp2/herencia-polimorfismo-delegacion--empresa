2015d68f7f7b2e2bdf36ebab0db81ce4ed805cf5
Leandro Doctors <ldoctors@untref.edu.ar>
2021-04-29 16:08:57 -0300

    Refactorizar: diferenciar Gerentes de Empleados Sin Categoría

---
+++ src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/SinCategoria.java
@@ -0,0 +1,9 @@
+package ar.edu.untref.ayp2.polimorfismo.empresa;
+
+public class SinCategoria extends Empleado {
+
+	public SinCategoria(String nombre, String categoria, String planta) {
+		super(nombre, categoria, planta);
+	}
+
+}
