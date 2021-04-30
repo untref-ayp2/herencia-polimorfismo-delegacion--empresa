b8d77fd26f9a0f92d7356984bf1515a7bc6cf508
Leandro Doctors <ldoctors@untref.edu.ar>
2021-04-30 16:37:16 -0300

    Refactorizar: "Categoria" en Empleade y Gerente sólo puede ser la propia
    Para asegurarnos de que sólo podemos crear Empleades/Gerentes con la
    categoría apropiada, eliminamos el parámetro del constructor, y lo
    forzamos al invocar el constructor de la superclase.

--- src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/Empleade.java
+++ src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/Empleade.java
@@ -3,8 +3,8 @@ package ar.edu.untref.ayp2.polimorfismo.empresa;
 public class Empleade extends EmpleadeAbstracte {
 	private String jornada;
 
-	public Empleade(String nombre, Categoria categoria, String planta, String jornada) {
-		super(nombre, categoria, planta);
+	public Empleade(String nombre, String planta, String jornada) {
+		super(nombre, Categoria.SIN_CATEGORIA, planta);
 		this.jornada = jornada;
 	}
 
