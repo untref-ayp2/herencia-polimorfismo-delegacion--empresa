a03717b07417b7b527be9c0f51dceba768502391
Leandro Doctors <ldoctors@untref.edu.ar>
2021-05-02 18:31:47 -0300

    la Empresa debe poder obtener un sueldo de Gerente (sin Antigüedad)
    Recién ahora utilizamos polimorfismo: para calcular su sueldo, la
    subclase "Gerente" utiliza el resultado de lo calculado por la
    superclase abstracta.
    
    Para simplificar el cálculo, por ahora evitamos incluir la antigüedad en
    nuestro test.
    
    Una vez más, extraemos el valor de la asignación por subalterno como
    constante para facilitar su posterior modificación.
    
    Además, volvemos a utilizar la anotación "@Override" para sobreescribir
    el método "obtSueldo()".

--- src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/Gerente.java
+++ src/main/java/ar/edu/untref/ayp2/polimorfismo/empresa/Gerente.java
@@ -2,6 +2,8 @@ package ar.edu.untref.ayp2.polimorfismo.empresa;
 
 public class Gerente extends EmpleadeAbstracte {
 
+	private static final double ASIGNACION_POR_SUBALTERNOS = 2000.0;
+
 	public Gerente(String nombre, boolean tienePareja, int cantHijes, int antiguedad) {
 		super(nombre, Categoria.GERENTE, Planta.PERMANENTE, Jornada.COMPLETA, tienePareja, cantHijes, antiguedad);
 	}
@@ -11,6 +13,11 @@ public class Gerente extends EmpleadeAbstracte {
 		String catyP = String.join(", ", categoria.toString(), planta.toString());
 		catyP = "(" + catyP + ")";
 		return String.join(" ", nombre, catyP);
+	}
+
+	@Override
+	public double obtSueldo() {
+		return super.obtSueldo() + ASIGNACION_POR_SUBALTERNOS;
 	}
 
 }
